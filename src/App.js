b90c5305b876ca3539f62bb6fa34ef0b1455ce25
Richard <richard.yang.tong@gmail.com>
2021-03-15 11:45:52 -0400

    Seperated different bootstrap components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import NavBar from './components/NavBar'
-import HelloBootstrap from './components/HelloBootstrap';
+import NavBar from './components/NavBar';
+import Card from './components/Card';
+import Jumbotron from './components/Jumbotron';
 
 function App() {
   return (
     <div>
       <h1>Hello world!</h1>
+      <Jumbotron />
       <NavBar />
-      <HelloBootstrap />
+      <Card />
     </div>
   );
 }
