f228ab7ca4edfc9fd7756b7a03af6a8ee5b4200d
Richard <richard.yang.tong@gmail.com>
2021-03-15 12:01:24 -0400

    Added a line for displyaing name without vowels.

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -3,12 +3,24 @@ import React from "react";
 function JSXVariables() {
     const name = 'Richard';
     const thoughts = 'is so amazing!';
+    const stripVowels = str => {
+        const vowels = ["a", "e", "i", "o", "u"];
+        let result = "";
+      
+        for (let i = 0; i < str.length; i++) {
+            if (!vowels.includes(str[i].toLowerCase())) {
+                result += str[i];
+            }
+        }
+        return result;
+    };
     return (
         <div className="main-container">
         <div className="container">
             <div className="jumbotron">
             <h1>Hi! My name is {name}!</h1>
             <h2>My name has {name.length} letters</h2>
+            <h2>My name without any vowel is: {stripVowels(name)}</h2>
             <h2>I think React {thoughts}</h2>
             </div>
         </div>
