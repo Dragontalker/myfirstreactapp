30e1fc0580a610fa8eb6a1a75193c2f7adb3e519
Richard <richard.yang.tong@gmail.com>
2021-03-15 11:53:13 -0400

    Added my thought in JSX.

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -2,13 +2,14 @@ import React from "react";
 
 function JSXVariables() {
     const name = 'Richard';
+    const thoughts = 'is so amazing!';
     return (
         <div className="main-container">
         <div className="container">
             <div className="jumbotron">
             <h1>Hi! My name is {name}!</h1>
-            <h2>My name has (insert number of letters in name here) letters</h2>
-            <h2>I think React (insert thoughts about React here)</h2>
+            <h2>My name has {name.length} letters</h2>
+            <h2>I think React {thoughts}</h2>
             </div>
         </div>
         </div>
