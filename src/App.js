4eb913b0dc5057ddaa6dc7eca52d79bc905950c5
Richard <richard.yang.tong@gmail.com>
2021-03-15 11:34:18 -0400

    Completed activity-4-HelloBootstrap.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
+import HelloBootstrap from './components/HelloBootstrap';
 
 function App() {
   return (
     <div>
       <h1>Hello world!</h1>
       <NavBar />
+      <HelloBootstrap />
     </div>
   );
 }
